52812d0ec1dabdfc18a726b75a5d3f785bdc911f
Andreas <74594568+Relouding@users.noreply.github.com>
2021-08-18 18:54:16 +0200

    Add files via upload

--- js/jackets.js
+++ js/jackets.js
@@ -13,7 +13,7 @@ async function getProducts(url){
 
     products.forEach(function(product){
         productContainer.innerHTML += `
-        <div class="product-new"><h1>${product.name}</h1>
+        <div class="product-new"><h2>${product.name}</h2>
         <div class="product-image-new" style="background-image:url('${product.image}')>
         </div></div>
         `
