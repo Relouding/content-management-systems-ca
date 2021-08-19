ca02c2072525875a3c69978284ebaea27a8b2cd5
Andreas <74594568+Relouding@users.noreply.github.com>
2021-08-19 16:42:17 +0200

    Add files via upload

--- js/jackets.js
+++ js/jackets.js
@@ -1,11 +1,23 @@
 const baseUrl = "https://wordpress.relouding.eu/wp-json/wc/store/products"
 const productContainer = document.querySelector(".product-container-new")
+const loader = document.getElementById("loader");
+getData();
 
-fetch("https://wordpress.relouding.eu/wp-json/wc/store/products")
-.then(response => response.json())
-.then(data => {
-console.log(data)
-})
+function getData() {
+  fetch("https://wordpress.relouding.eu/wp-json/wc/store/products", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        getProducts(data[i]);
+      }
+      loader.style.display = "none";
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 async function getProducts(url){
     const response = await fetch(url);
@@ -13,7 +25,7 @@ async function getProducts(url){
 
     products.forEach(function(product){
         productContainer.innerHTML += `
-        <div class="product-new"><h2>${product.name}</h2>
+        <div class="product-new"><h2 class="h3">${product.name}</h2>
         <div class="product-image-new" style="background-image:url('${product.image}')>
         </div></div>
         `
