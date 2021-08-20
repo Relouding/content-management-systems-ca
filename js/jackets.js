4420f3d7482c127d947dcfe7159fe6a00be5a43d
Andreas <74594568+Relouding@users.noreply.github.com>
2021-08-20 20:19:54 +0200

    Add files via upload

--- js/jackets.js
+++ js/jackets.js
@@ -1,23 +1,6 @@
 const baseUrl = "https://wordpress.relouding.eu/wp-json/wc/store/products"
 const productContainer = document.querySelector(".product-container-new")
-const loader = document.getElementById("loader");
-getData();
 
-function getData() {
-  fetch("https://wordpress.relouding.eu/wp-json/wc/store/products", {
-    method: "GET",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        getProducts(data[i]);
-      }
-      loader.style.display = "none";
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
 
 async function getProducts(url){
     const response = await fetch(url);
@@ -25,9 +8,16 @@ async function getProducts(url){
 
     products.forEach(function(product){
         productContainer.innerHTML += `
-        <div class="product-new"><h2 class="h3">${product.name}</h2>
-        <div class="product-image-new" style="background-image:url('${product.image}')>
-        </div></div>
+
+        <div class="product-new">
+        <img src="${product.images[0].src}" alt"${product.name}">
+        <div class="nam-new">${product.name}</div>
+        <div class="des-new">Description: ${product.short_description}</div>
+        <div class="pri-new">Price: ${product.prices.price} $</div>
+        <a href="jacketinfo.html?id=${product.id}" class="button see-more-new">More Details</a>
+        <a href="#0" class="button add-to-cart-new" data-id=${product.id}>Add to cart</a>
+        </div>
+
         `
     })
 }
